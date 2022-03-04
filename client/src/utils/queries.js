6e7add19ad34ecb032872a3c6e138a29625b13e5
mavisyupyup1 <mavisyupyup1@github.com>
2022-03-04 09:10:11 -0500

    added queries. js me

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,18 @@
 import {gql} from "@apollo/client"
+export const GET_ME =gql`
+{
+    me{
+        _id
+        username
+        bookCount
+       addBook {
+        bookId
+            authors
+            description
+            title
+            image
+            link
+        }
+    }
+}
+`
